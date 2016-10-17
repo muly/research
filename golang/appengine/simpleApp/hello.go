ba2dee7ab06337da025c51a8490043b88474c7bd
srinivasa reddy muly <msrinivasareddy@gmail.com>
2016-10-17 13:38:40 -0400

    appengine-golang-test simplified the appengine SDK guestbook example to a RESTful-like API

--- golang/appengine/simpleApp/hello.go
+++ golang/appengine/simpleApp/hello.go
@@ -1,7 +1,8 @@
 package guestbook
 
 import (
-	"html/template"
+	//"html/template"
+	"encoding/json"
 	"net/http"
 	"time"
 
@@ -32,6 +33,10 @@ func guestbookKey(c appengine.Context) *datastore.Key {
 
 // [START func_root]
 func root(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "", http.StatusNotFound)
+		return
+	}
 	c := appengine.NewContext(r)
 	// Ancestor queries, as shown here, are strongly consistent with the High
 	// Replication Datastore. Queries that span entity groups are eventually
@@ -47,45 +52,31 @@ func root(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// [END getall]
-	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+	if err := json.NewEncoder(w).Encode(greetings); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
 // [END func_root]
 
-var guestbookTemplate = template.Must(template.New("book").Parse(`
-<html>
-  <head>
-    <title>Go Guestbook</title>
-  </head>
-  <body>
-    {{range .}}
-      {{with .Author}}
-        <p><b>{{.}}</b> wrote:</p>
-      {{else}}
-        <p>An anonymous person wrote:</p>
-      {{end}}
-      <pre>{{.Content}}</pre>
-    {{end}}
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`))
-
 // [START func_sign]
 func sign(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "", http.StatusNotFound)
+		return
+	}
 	// [START new_context]
 	c := appengine.NewContext(r)
-	// [END new_context]
-	g := Greeting{
-		Content: r.FormValue("content"),
-		Date:    time.Now(),
+
+	g := Greeting{}
+	if err := json.NewDecoder(r.Body).Decode(&g); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	g.Date = time.Now()
+
 	// [START if_user]
 	if u := user.Current(c); u != nil {
 		g.Author = u.String()
@@ -100,8 +91,7 @@ func sign(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, "/", http.StatusFound)
-	// [END if_user]
+
 }
 
 // [END func_sign]
