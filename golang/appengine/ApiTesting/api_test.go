7fadd5c29477b8b39b3eb23b752e15fa13755e78
srinivasa reddy muly <msrinivasareddy@gmail.com>
2016-10-29 02:23:32 -0400

    further simplified version to highlight the error

--- golang/appengine/ApiTesting/api_test.go
+++ golang/appengine/ApiTesting/api_test.go
@@ -2,59 +2,23 @@ package logr
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"google.golang.org/appengine"
 )
-
-var (
-	testserver *httptest.Server
-	goalUrl    string
-)
-
-func init() {
-	testserver = httptest.NewServer(Handlers())
-
-	goalUrl = testserver.URL + "/goal/123"
-
-}
 
 func Test123(t *testing.T) {
 
-	//inst, err := aetest.NewInstance(nil) // ERROR: "appengine: NewContext passed an unknown http.Request"
-	//inst, err := aetest.NewInstance(&aetest.Options{}) // ERROR: "appengine: NewContext passed an unknown http.Request"
-	//inst, err := aetest.NewInstance(&aetest.Options{AppID: "testApp", StronglyConsistentDatastore: true}) // ERROR: Unable to assign value 'testApp' to attribute 'application'. Value 'testApp' for application does not match expression...
-	//inst, err := aetest.NewInstance(&aetest.Options{AppID: "testapp", StronglyConsistentDatastore: true}) // ERROR: "appengine: NewContext passed an unknown http.Request"
-	//inst, err := aetest.NewInstance(&aetest.Options{AppID: "testapp", StronglyConsistentDatastore: false}) // ERROR: "appengine: NewContext passed an unknown http.Request"
-
-	/*if err != nil {
-		t.Fatalf("Failed to create instance: %v", err)
-	}
-	defer inst.Close()*/
-
 	body := strings.NewReader("")
 
-	request, err := http.NewRequest("GET", goalUrl, body) //inst.NewRequest("GET", goalUrl, body) //
+	request, err := http.NewRequest("GET", "", body) //inst.NewRequest("GET", goalUrl, body) //
 	if err != nil {
 		t.Error(err)
 	}
 	t.Log(request)
 
-	//c := appengine.NewContext(request)
-	//t.Log(c)
-
-	res, err := http.DefaultClient.Do(request) // ERROR: "appengine: NewContext passed an unknown http.Request"
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	t.Log(res)
-
-	t.Log(goalUrl)
-
-	/*if res.StatusCode != 201 {
-		t.Errorf("Success expected: %d", res.StatusCode)
-	}*/
+	c := appengine.NewContext(request) // ERROR: appengine: NewContext passed an unknown http.Request
+	t.Log(c)
 
 }
