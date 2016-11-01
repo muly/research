336c3e8e01edec79bea68b02b26206b1db0cbaac
srinivasa reddy muly <msrinivasareddy@gmail.com>
2016-11-01 18:35:01 -0400

    seperate app to test: fixed the issue with http.DefaultClient by using functions from package appengine/urlfetch.
    next : need to try to see if these functions will help in the unit test code instead of having to write a seperate app

--- golang/appengine/ApiTesting2/router.go
+++ golang/appengine/ApiTesting2/router.go
@@ -6,8 +6,12 @@ import (
 	"encoding/json"
 	//"fmt"
 	"net/http"
+	"time"
 
 	//"ctrl"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 
 	"github.com/gorilla/mux"
 )
@@ -40,25 +44,52 @@ func init() {
 
 }
 
+type Goal struct {
+	Name       string
+	Notes      string `json:"Notes,omitempty"`
+	CreatedOn  time.Time
+	ModifiedOn time.Time `json:"ModifiedOn,omitempty"`
+}
+
 func HandleApiTest(w http.ResponseWriter, r *http.Request) {
 	//c := appengine.NewContext(r)
+	/*
+		req, err := http.NewRequest("GET", goalUrl, nil)
+		if err != nil {
+			http.Error(w, "Error with http.NewRequest():"+err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	req, err := http.NewRequest("GET", goalUrl, nil)
-	if err != nil {
-		http.Error(w, "Error with http.NewRequest():"+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		http.Error(w, "Error with http.DefaultClient.Do():"+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			http.Error(w, "Error with http.DefaultClient.Do():"+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	*/
 	//fmt.Println(res)
 
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+	res, err := client.Get(goalUrl)
+	if err != nil {
+		http.Error(w, "Error with client.Get(goalUrl):"+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	goal := Goal{}
+
+	if err := json.NewDecoder(res.Body).Decode(&goal); err != nil {
+		http.Error(w, "Error with json.NewDecoder(r.Body).Decode(&goal):"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	if err := json.NewEncoder(w).Encode(res.Status); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Error with json.NewEncoder(w).Encode(res.Status):"+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(goal); err != nil {
+		http.Error(w, "Error with json.NewEncoder(w).Encode(goal): "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
