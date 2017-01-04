7ad11148fe2f42162a16ca261cf98012bb36cb9e
srinivasa reddy muly <msrinivasareddy@gmail.com>
2017-01-03 19:11:31 -0500

    wip code for another method to identify if a type implements the given interface

--- golang/interface/does-this-type-implement-interface.go
+++ golang/interface/does-this-type-implement-interface.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type myInterface interface {
 	IsValid() bool
 }
@@ -17,10 +21,18 @@ func (k myFuncType1) IsValid1() bool {
 }
 
 func main() {
-	// quick and dirty method:
+	// method 1: quick and dirty method:
 	//	if below compiles sucessfully then the type `myFuncType` implement the `myInterface` interface
 	//  source: http://goblog.qwest.io/2016/05/functions-can-implement-interface-in.html
-	var _ myInterface = (*myFuncType)(nil)  // no error
-	var _ myInterface = (*myFuncType1)(nil) // ERROR: *myFuncType1 does not implement myInterface (missing IsValid method)
+	var _ myInterface = (*myFuncType)(nil) // no error
+	//var _ myInterface = (*myFuncType1)(nil) // ERROR: *myFuncType1 does not implement myInterface (missing IsValid method)
+
+	// method 2: to find out without compilation error
+	//
+	//  source: https://youtu.be/yeetIgNeIkc?t=15m33s
+	var v interface{}
+	v = myFuncType{}
+	obj, ok := v.(myInterface)
+	fmt.Println(ok)
 
 }
